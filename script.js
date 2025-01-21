64324d2bb477f1a3f9a4013f7d1cd362b089c882
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-01-21 23:26:13 +0500

    style added

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  dots: true,
+  focusOnSelect: true,
+  centerMode: true,
+});
