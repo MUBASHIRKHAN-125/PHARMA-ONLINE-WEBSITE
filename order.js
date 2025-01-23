eb86605148cb5bae10fde4a645d5018d590dd2fd
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-01-23 21:34:14 +0500

    responsive

---
+++ order.js
@@ -0,0 +1,37 @@
+const Messagesend = () => {
+(function(){
+        emailjs.init({
+          publicKey: "Ql5XR0NamqOZmaaX3",
+        });
+     })();
+
+     let firstName = document.getElementById("firstname").value;
+     let lastName = document.getElementById("lastname").value;
+     let userEmail= document.getElementById("email").value;
+     let userMess = document.getElementById("message").value;
+     let usersub = document.getElementById("subject").value;
+
+     if(!firstName , !lastName ,!userEmail , !userMess , !usersub ){
+        alert("... PLEASE FILL INPUT ...");
+        return;
+     }
+
+     let userDetail = {
+        first_name : firstName,
+        last_name : lastName,
+        email : userEmail,
+        message : userMess,
+        subject : usersub
+     }
+     emailjs.send("service_e4j4fue" , "template_tf36zko" , userDetail)
+.then((response) => {
+    alert("ORDER COMPLETE YOUR ITEM 24 HOURS DELEVER");
+         console.log(response);
+      window.location.href="index.html";
+})
+.catch((error) => {
+    console.log(error);
+})
+}
+
+
--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-$('.multiple-items').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1000,
-  dots: true,
-  focusOnSelect: true,
-  centerMode: true,
+$(document).ready(function() {
+  $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,  
+      slidesToScroll: 1,
+      prevArrow: false, 
+      nextArrow: false, 
+      autoplay: true,  
+      autoplaySpeed: 1000, 
+      dots: true,         
+      focusOnSelect: true, 
+      centerMode: true, 
+      responsive: [
+          {
+              breakpoint: 770,
+              settings: {
+                  slidesToShow: 2,  
+                  slidesToScroll: 1
+              }
+          },
+          {
+              breakpoint: 580,
+              settings: {
+                  slidesToShow: 1,  
+                  slidesToScroll: 1
+              }
+          }
+      ]
+  });
 });
